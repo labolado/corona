9f65cdcc60ee4381f7d19d7e0f1bc0a38a8dae35
Labo Lado <laboladoapp@gmail.com>
2026-04-18 01:22:25 +0800

    Fix bgfx-demo config: use 320x480 content area for proper object sizing

--- tests/bgfx-demo/config.lua
+++ tests/bgfx-demo/config.lua
@@ -7,8 +7,8 @@
 
 application = {
     content = {
-        width = 1280,
-        height = 1920,
+        width = 320,
+        height = 480,
         scale = "letterbox",
         fps = 60,
         imageSuffix = {}
